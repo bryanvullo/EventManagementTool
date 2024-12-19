0fc38f004044daa48c0cffcbdfee1cfd9be585ec
bryanvullo <bryanvullo04@gmail.com>
2024-12-19 17:08:07 +0000

    function to create events using GPT

--- evecs-db/function_app.py
+++ evecs-db/function_app.py
@@ -1,5 +1,6 @@
 import logging
 import json
+import jsonschema
 import os
 import requests
 
@@ -10,6 +11,7 @@ from openai import AzureOpenAI
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
+# Cosmos DB Containers
 GroupCosmos = CosmosClient.from_connection_string(os.environ['DB_CONNECTION_STRING'])
 EvecsDBProxy = GroupCosmos.get_database_client(os.environ['DB_NAME'])
 EventsContainerProxy = EvecsDBProxy.get_container_client(os.environ['EVENTS_CONTAINER'])
@@ -17,23 +19,64 @@ TicketsContainerProxy = EvecsDBProxy.get_container_client(os.environ['TICKETS_CO
 LocationsContainerProxy = EvecsDBProxy.get_container_client(os.environ['LOCATIONS_CONTAINER'])
 UsersContainerProxy = EvecsDBProxy.get_container_client(os.environ['USERS_CONTAINER'])
 
-@app.route(route="placeholder_function")
-def placeholder_function(req: func.HttpRequest) -> func.HttpResponse:
+# Azure OpenAI
+OpenAIEndpoint = os.environ['OPENAI_ENDPOINT']
+OpenAIKey = os.environ['OPENAI_API_KEY']
+OpenAIApiVersion = "2024-08-01-preview"
+OpenAIClient = AzureOpenAI(azure_endpoint=OpenAIEndpoint, api_key=OpenAIKey, api_version=OpenAIApiVersion)
+    
+@app.route(route="create_event_gpt", auth_level=func.AuthLevel.FUNCTION, methods=['POST'])
+def createEventGPT(req: func.HttpRequest) -> func.HttpResponse:
+    """
+    Creates a new event using GPT-35-turbo
+    input: text
+    output: Event: json object
+    """
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    input = req.get_json()
+    text = input['text']
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+    with open('schemas/event.json', 'r') as f:
+        schema = f.read()
+
+    eventJSON = ""
+    prompt = f'''
+        Using the following text as input, create a new event as a JSON object 
+        in the structure of the following JSON Schema:
+        
+        Input: 
+        {text}
+
+        JSON Schema:
+        {schema}
+        '''
+    valid = False
+
+    for i in range(4):
+        eventJSON = OpenAIClient.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": 
+                    "Assistant is a large language model trained to create JSON objects based on user input. "},
+                {"role": "user", "content": prompt}
+            ]
         )
+
+        try: 
+            jsonschema.validate(instance=eventJSON, schema=schema)
+            valid = True
+            break
+        except jsonschema.exceptions.ValidationError as e:
+            logging.error(f"Error: {e.message}")
+
+    if not valid:
+        return func.HttpResponse(
+            body = json.dumps({"result" : "Cannot generate JSON" }),
+            status_code=500
+        )
+    
+    return func.HttpResponse(
+        body = json.dumps(eventJSON),
+        status_code=200
+    )
