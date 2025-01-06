09a2843e55d56e659da79799d3c9a936bf45783b
bryanvullo <bryanvullo04@gmail.com>
2025-01-06 22:36:23 +0000

    test login complete

--- evecs-db/testing/testLogin.py
+++ evecs-db/testing/testLogin.py
@@ -9,7 +9,7 @@ from pathlib import Path
 class TestRegisterPlayer(unittest.TestCase):   
     LOCAL_DEV_URL = "http://localhost:7071/api/"
     PUBLIC_URL = "https://evecs.azurewebsites.net/api/"
-    TEST_FUNCTION = "register_user"
+    TEST_FUNCTION = "login_user"
     TEST_URL = LOCAL_DEV_URL + TEST_FUNCTION
     
     path = Path(__file__).parent.parent / 'local.settings.json'
@@ -20,11 +20,6 @@ class TestRegisterPlayer(unittest.TestCase):
     EvecsDBProxy = MyCosmos.get_database_client(settings['Values']['DB_NAME'])
     UserContainerProxy = EvecsDBProxy.get_container_client(settings['Values']['USERS_CONTAINER'])
     FunctionAppKey = settings['Values']['FUNCTION_APP_KEY']
-
-    validPlayer = {
-        "email": "bryanvullo@gmail.com",
-        "password": "password123!!"
-    }
 
     testUser1 = {
         "email": "test-user@gmail.com",
@@ -40,7 +35,7 @@ class TestRegisterPlayer(unittest.TestCase):
     }
 
     @classmethod
-    def tearDownClass(cls):
+    def setUpClass(cls):
         response = requests.post(cls.TEST_URL, json=cls.testUser1, 
                                  headers={"x-functions-key": cls.FunctionAppKey})
         response = requests.post(cls.TEST_URL, json=cls.testUser2, 
@@ -48,16 +43,68 @@ class TestRegisterPlayer(unittest.TestCase):
         response = requests.post(cls.TEST_URL, json=cls.testUser3, 
                                  headers={"x-functions-key": cls.FunctionAppKey})
 
-    def tearDown(self):
-        for doc in self.UserContainerProxy.read_all_items():
-          self.UserContainerProxy.delete_item(item=doc, partition_key=doc['user_id'])
-
-    def test_register_valid_player(self):
-        response = requests.post(self.TEST_URL, json=self.validPlayer, 
+    def test_login_valid_user1(self):
+        response = requests.post(self.TEST_URL, json=self.testUser1, 
                                  headers={"x-functions-key": self.FunctionAppKey})
 
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.json()["result"], f"User '{self.validPlayer.get('email')}' has been registered.")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["result"], f"User '{self.testUser1.get('email')}' has been logged in.")
+    
+    def test_login_valid_user2(self):
+        response = requests.post(self.TEST_URL, json=self.testUser2, 
+                                 headers={"x-functions-key": self.FunctionAppKey})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["result"], f"User '{self.testUser2.get('email')}' has been logged in.")
+    
+    def test_empty_login(self):
+        response = requests.post(self.TEST_URL, json={}, 
+                                 headers={"x-functions-key": self.FunctionAppKey})
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json()["error"], "Email and password are required.")
+    
+    def test_empty_email(self):
+        user = {
+            "password": "password123!!"
+        }
+        response = requests.post(self.TEST_URL, json=user, 
+                                 headers={"x-functions-key": self.FunctionAppKey})
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json()["error"], "Email and password are required.")
+    
+    def test_empty_password(self):
+        user = {
+            "email": "test-user@gmail.com"
+        }
+        response = requests.post(self.TEST_URL, json=user, 
+                                 headers={"x-functions-key": self.FunctionAppKey})
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json()["error"], "Email and password are required.")
+    
+    def test_invalid_email(self):
+        user = {
+            "email": "invalid@gmail.com",
+            "password": "password123!!"
+        }
+        response = requests.post(self.TEST_URL, json=user, 
+                                 headers={"x-functions-key": self.FunctionAppKey})
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json()["error"], f"User with email '{user.get('email')}' not found.")
+    
+    def test_invalid_password(self):
+        user = {
+            "email": "test-user@gmail.com",
+            "password": "wrongpassord!!"
+        }
+        response = requests.post(self.TEST_URL, json=user, 
+                                 headers={"x-functions-key": self.FunctionAppKey})
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json()["error"], "Password is incorrect.")
 
     if __name__ == '__main__':
         unittest.main()
